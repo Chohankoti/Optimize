b5e92accdaf14c42a109ba193740102857646b77
unknown <chohankoti333@gmail.com>
2024-03-20 08:43:37 +0530

    working on image

--- src/Components/Pages/UnOptimizedImage.jsx
+++ src/Components/Pages/UnOptimizedImage.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-export default function UnOptimizedImage() {
+
+
+const UnOptimizedImage = () => {
+  const images = Array.from({ length: 30 }, (_, index) => ({
+    [`image${index + 1}`]: `../../assets/images/image${index + 1}.jpg`,
+  }));
+  console.log(images)
   return (
-    <div className="flex justify-center items-center h-screen">UnOptimizedImage</div>
-  )
-}
+    <div className="grid grid-cols-3 gap-4">
+      {Object.keys(images).map((imageName) => (
+        <div key={imageName} className="aspect-w-1 aspect-h-1">
+          <img src={images[imageName]} alt={`Image ${imageName}`} className="object-cover w-full h-full rounded-lg" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UnOptimizedImage;
